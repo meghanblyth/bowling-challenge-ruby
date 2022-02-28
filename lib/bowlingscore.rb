fb8613d51a93949ed144abac6a87b7b07d15a3f6
meghanblyth <megblyth@gmail.com>
2022-02-28 11:02:58 +0000

    Refactoring the class and tests

--- lib/bowlingscore.rb
+++ lib/bowlingscore.rb
@@ -1,19 +1,14 @@
 class Bowlingscore
+
+  attr_reader :total 
+
   def initialize
-    @rolls = []
-  end
+    @total = 0
+  end 
+ 
+  def roll(pin)
+    pin 
+    @total += pin 
+  end 
 
-  def roll(points)
-    @rolls << points
-  end
-
-  def score
-    scores = 0
-    roll_turn = 0
-    20.times do
-      result += @rolls[roll_turn]
-      roll_turn += 1
-    end
-    result
-  end
-end
+end 
--- spec/bowlingscore_spec.rb
+++ spec/bowlingscore_spec.rb
@@ -2,21 +2,32 @@ require_relative '../lib/bowlingscore.rb'
 require 'rspec'
 
 describe Bowlingscore do
-  let(:bowlingscore) { Bowlingscore.new }
 
-  describe '#initialize' do
-    it 'initialize a new bowlingscore instance' do
-      expect(bowlingscore).to be_a Bowlingscore
-    end
-  end
+  let(:bowlingscore) { described_class.new }
 
-  it 'can roll a gutter game' do
-    20.times { @bowlingscore.roll(0) }
-    expect(@bowlingscore.score).to eq(0)
-  end
+  describe '#roll' do 
+    it 'returns 5 if you roll 5' do 
+      expect(bowlingscore.roll(5)).to eq 5
+    end 
 
-  it 'can roll all ones' do
-    20.times { @bowlingscore.roll(1) }
-    expect(@bowlingscore.score).to eq(20)
-  end
-end
+    it 'returns 7 if you roll 7' do 
+      expect(bowlingscore.roll(7)).to eq 7
+    end 
+
+    it 'returns the number of pins that were knocked down' do 
+      expect(bowlingscore.roll(6)).to eq 6 
+    end 
+
+    it 'adds pin to the score total' do 
+      expect{ bowlingscore.roll(8) }.to change { bowlingscore.total }.by 8 
+    end 
+
+    it 'adds multiple rolls to the score total' do
+      bowlingscore.roll(5)
+      bowlingscore.roll(6)
+      bowlingscore.roll(7)
+      expect(bowlingscore.total).to eq 18 
+    end 
+  end 
+end 
+
